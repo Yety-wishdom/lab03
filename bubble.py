6ce74d251baf2be593f010ad2fe4c2829184b0c6
Yety-wishdom <satanpower4225@gmail.com>
2023-10-25 07:39:49 +0500

    Обновленная сортировка

--- bubble.py
+++ bubble.py
@@ -1,3 +1,6 @@
+print("Введите вариант сортировки (по возрастанию / по убыванию):")
+s = input()
+
 n = int(input("Введите количество чисел: "))
 print("Введите числа:")
 
@@ -8,13 +11,20 @@ for i in range(n):
 
 print("Исходный массив:")
 print(mas)
-
-for i in range(n - 1):
-    for j in range(n - i - 1):
-        if mas[j] > mas[j + 1]:
-            tmp = mas[j]
-            mas[j] = mas[j + 1]
-            mas[j + 1] = tmp
+if (s == "по возрастанию"):
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if mas[j] > mas[j + 1]:
+                tmp = mas[j]
+                mas[j] = mas[j + 1]
+                mas[j + 1] = tmp
+if (s == "по убыванию"):
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if mas[j] < mas[j + 1]:
+                tmp = mas[j]
+                mas[j] = mas[j + 1]
+                mas[j + 1] = tmp
 
 print("Массив после сортировки пузырьком:")
 print(mas)
